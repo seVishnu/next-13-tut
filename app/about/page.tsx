b5d00c55d773cb12428f6c25e13957c78c121c2f
vishnuSekhar <vishnusekhar96@gmail.com>
2023-02-27 13:30:33 +0530

    fea

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+function About() {
+  //   throw new Error("new error");
+  return (
+    <>
+      <h1>About</h1>
+      <Link href="/">Go to Home</Link>
+    </>
+  );
+}
+export default About;
